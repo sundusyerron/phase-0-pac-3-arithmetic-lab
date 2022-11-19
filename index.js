11adeca1679011ade6cc961f9397d8c78aac15ea
Sundus <sundusaden105@gmail.com>
2022-11-20 02:15:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(x,y){
+    return x+y;
+    }
+    function subtract(x,y){
+    return x-y;
+    }
+    function multiply(x,y){
+    return x*y;
+    }
+    function divide(x,y){
+    return x/y;
+    }
+    function increment(x){
+    return x+1;
+    }
+    function decrement(x){
+    return x-1;
+    }
+    function makeInt(x){
+    let y = parseInt(x,10);
+    return y;
+    }
+    function preserveDecimal(x){
+    let y = parseFloat(x);
+    return y;
+    }
